3bf0578d8ce3f7d303346db5cfc92906892eaaf5
woutermaessen <woutermaessen2511@gmail.com>
2025-10-18 16:35:19 +0200

    print for uWSGI

--- routes.py
+++ routes.py
@@ -10,7 +10,16 @@ import subprocess
 import glob
 from threading import Thread
 import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info("This is an info message")
+logger.error("This is an error message")
+
+print("Hello world", flush=True)
+print("Hello world", flush=True)
 
 progress = 0
 status_message = "Booting Script..."
